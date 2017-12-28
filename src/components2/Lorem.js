a1924a31b40ee46d22bf2ae4ff080d3d48b16cea
dxstarr <dxstarr@godaddy.com>
2017-12-27 19:45:10 -0600

    Lorem Component is solid

--- src/components2/Lorem.js
+++ src/components2/Lorem.js
@@ -9,7 +9,6 @@ const Lorem = ({title, match}) => {
     return (
         <div>
             <h1>{title}</h1>
-            <p><strong>Time Stamp:</strong> {Date.now()}</p>
             <p><strong>Requested Path:</strong> {match.path}</p>
             <p>
                 Lorem ipsum dolor sit amet, sit dicit oblique blandit eu, vis impetus deserunt te. Mutat simul vivendum te qui, no aperiri principes mel. Nam unum accusam contentiones ex, tempor euismod ei est, at pri omnis vocent denique. Et lorem sadipscing quo, per doming argumentum complectitur ad.
@@ -17,6 +16,8 @@ const Lorem = ({title, match}) => {
             <p>
                 Veri torquatos usu cu, id mandamus convenire adolescens has. Mea nisl autem at, iuvaret euismod cum ut, quo porro dolorem no. Ei sea viderer suavitate tincidunt, minimum fabellas te usu, abhorreant consequuntur ea cum. Ne exerci democritum dissentias nam, te eos eloquentiam efficiantur.
             </p>
+            <p><strong>Time Stamp:</strong> {Date.now()}</p>
+
         </div>
     );
 };
