e89db4b316f7f87c2d17212f6f3a463a6beb5a3f
dxstarr <dxstarr@godaddy.com>
2018-01-18 12:50:04 -0600

    made links bigger

--- src/components/customLink/Logging.js
+++ src/components/customLink/Logging.js
@@ -8,13 +8,15 @@ const Logging = (props) => {
     return (
         <div>
             <h1>Logging Links</h1>
-            <LoggingLink to={`${props.match.url}/lorem/7fd2a0f4-29ea-4650-803a-67418b597090`}>Link 1</LoggingLink>
-            {' | '}
-            <LoggingLink to={`${props.match.url}/lorem/7842a9f0-449d-499f-bd19-36106da591fa`}>Link 2</LoggingLink>
+            <h4>
+                <LoggingLink to={`${props.match.url}/lorem/7fd2a0f4-29ea-4650-803a-67418b597090`}>Link 1</LoggingLink>
+                {' | '}
+                <LoggingLink to={`${props.match.url}/lorem/7842a9f0-449d-499f-bd19-36106da591fa`}>Link 2</LoggingLink>
+            </h4>
 
-            <Route path={`${props.match.url}/lorem/:eid`} render={ () => {
-                return <Lorem />
-            }} />
+            <Route path={`${props.match.url}/lorem/:eid`} render={() => {
+                return <Lorem/>
+            }}/>
         </div>
     );
 
@@ -23,7 +25,9 @@ const Logging = (props) => {
 export default withRouter(Logging);
 
 
-
-{/*<LoggingLink to={`${props.match.url}/lorem/`} eid='7fd2a0f4-29ea-4650-803a-67418b597090'>Link 1</LoggingLink>*/}
-{/*{' | '}*/}
-{/*<LoggingLink to={`${props.match.url}/lorem/`} eid='7842a9f0-449d-499f-bd19-36106da591fa'>Link 2</LoggingLink>*/}
+{/*<LoggingLink to={`${props.match.url}/lorem/`} eid='7fd2a0f4-29ea-4650-803a-67418b597090'>Link 1</LoggingLink>*/
+}
+{/*{' | '}*/
+}
+{/*<LoggingLink to={`${props.match.url}/lorem/`} eid='7842a9f0-449d-499f-bd19-36106da591fa'>Link 2</LoggingLink>*/
+}
