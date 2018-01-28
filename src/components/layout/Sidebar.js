cab9b8ca95e1e263f4f19dc707ee79b6c4677b46
dxstarr <dxstarr@godaddy.com>
2018-01-28 12:39:40 -0800

    added NavLinks instead of Links

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,12 +1,12 @@
 import React from "react";
-import {Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 
 const Sidebar = () => {
 
     return (
         <div className={'leftNavContainer'}>
-            <Link to='/' className='navLink'>Home</Link><br/>
-            <Link to='/propview' className='navLink'>Prop Viewer</Link><br/>
+            <NavLink exact to='/' activeClassName='activeNavLink' className='navLink'>Home</NavLink><br/>
+            <NavLink to='/propview' activeClassName='activeNavLink' className='navLink'>Prop Viewer</NavLink><br/>
         </div>
     );
 };
