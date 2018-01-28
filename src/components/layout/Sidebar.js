dced766245722e446e3aed5d2cc6183d28d2e130
dxstarr <dxstarr@godaddy.com>
2018-01-28 15:28:46 -0800

    added color swatch and using array for navlinks

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -3,11 +3,26 @@ import {NavLink} from 'react-router-dom';
 
 const Sidebar = () => {
 
+    const links = [
+        {name: 'Home', url: '/'},
+        {name: 'Prop Viewer', url: '/propview'},
+        {name: '404 Error', url: '/404Error'},
+    ];
+
+    let linksComponents = links.map((link, index) => {
+        return (
+            <li key={index} className={'nav'}>
+                <NavLink className={'navLink'} activeClassName={'activeNavLink'}
+                         to={link.url} exact>{link.name}</NavLink>
+            </li>
+        );
+    });
+
     return (
         <div className={'leftNavContainer'}>
-            <NavLink exact to='/' activeClassName='activeNavLink' className='navLink'>Home</NavLink><br/>
-            <NavLink to='/propview' activeClassName='activeNavLink' className='navLink'>Prop Viewer</NavLink><br/>
-            <NavLink to='/404Error' activeClassName='activeNavLink' className='navLink'>Page Not Found</NavLink><br/>
+            <ul>
+                {linksComponents}
+            </ul>
         </div>
     );
 };
