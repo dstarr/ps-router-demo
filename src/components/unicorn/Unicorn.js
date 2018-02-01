09d94a0c7946d2c9f1f7f151cf089e58cb8417df
dstarr <david@elegantcode.com>
2018-02-01 13:43:39 -0800

    Adding Unicorn

---
+++ src/components/unicorn/Unicorn.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Link, Route} from "react-router-dom";
+
+const Unicorn = ({match}) => {
+
+    return (
+        <div>
+            <h1>Unicorn Love</h1>
+            <h4>
+                <Link to={`${match.url}`}>Left</Link>
+                {' | '}
+                <Link to={`${match.url}/arg`}>Right</Link>
+            </h4>
+
+            <Route path={`${match.url}/arg`} children={({match}) => {
+
+                let direction = match ? 'right' : 'left';
+
+                return (<img src={`/assets/unicorn_${direction}.png`} width={'400px'}/>)
+            }}/>
+
+        </div>
+    )
+};
+
+export default Unicorn;
