f00522b48be32fdc0ee6cedf2cd6edd56d9399d2
dstarr <david@elegantcode.com>
2018-02-05 16:52:43 -0800

    removing comments from Route definitions

--- src/App.js
+++ src/App.js
@@ -35,14 +35,10 @@ const App = () => {
                         <Route path='/prompt' component={NameForm}/>
                         <Route path='/logging' component={LoggingHome}/>
 
-                        // people
-
-                        // secure routes
                         <ProtectedRoute path={'/protected'} component={ProtectedHome}/>
                         <Route path={'/login'} component={Login}/>
                         <Route path={'/logout'} component={Logout}/>
 
-                        // page not found route
                         <Route component={PageNotFound}/>
                     </Switch>
 
