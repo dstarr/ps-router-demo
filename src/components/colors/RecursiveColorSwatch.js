fbcb3f5e96823227a6a3f5f8b09ab55435fca48b
dstarr <david@elegantcode.com>
2018-02-05 16:40:47 -0800

    removed propviewer and fixed recursive component bug.

--- src/components/colors/RecursiveColorSwatch.js
+++ src/components/colors/RecursiveColorSwatch.js
@@ -31,7 +31,6 @@ const RecursiveColorSwatch = ({match}) => {
                 <Link to={`${match.url}`}>Close Child</Link>
             </h4>
 
-            // here's the magic
             <Route path={`${match.url}/:color/:text`} component={RecursiveColorSwatch}/>
 
         </div>
