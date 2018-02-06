051982c3025635deeca055ed41ea3c24f46f7930
dstarr <david@elegantcode.com>
2018-02-05 16:42:46 -0800

    removed propviewer and fixed recursive component bug.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Header from "./components/layout/Header";
 import Sidebar from "./components/layout/Sidebar";
 import Home from './components/Home';
-import PropViewer from './components/PropViewer';
 import PageNotFound from './components/PageNotFound';
 import ColorSwatch from './components/colors/ColorSwatch';
 import NameForm from './components/prompt/NameForm';
@@ -30,7 +29,6 @@ const App = () => {
 
                     <Switch>
                         <Route path='/' component={Home} exact/>
-                        <Route path='/propview' component={PropViewer}/>
                         <Route path='/colors/:color/:text' component={RecursiveColorSwatch}/>
                         <Route path='/color' render={() => {
                             return <ColorSwatch text='Red' color='#ff0000'/>
