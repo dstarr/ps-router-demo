b61c5463382922056650a3c2d6300eb2c0ba50f9
dstarr <david@elegantcode.com>
2018-02-08 08:55:04 -0800

    Adding demo instructions to the App file.

--- src/App.js
+++ src/App.js
@@ -29,3 +29,26 @@ const App = () => {
 
 
 export default App;
+
+
+
+
+/*
+    0. npm install react-router-dom
+    1. Add HashRounter
+    2. Add Route to Home
+        - path is the router match.
+        - here we are matching on the application root
+        - so this is going to be our home page
+    3. Look at URL in Browser
+    4. Replace with BrowserRouter
+        - Look at URL in Browsers
+        - No hashtag
+    5. Add 4
+    6. Remove 4
+    7. Add /foo
+        - goto foo in Browser
+        - show 2 matches
+    8. Add Switch
+    9. Add Link to Sidebar
+ */
